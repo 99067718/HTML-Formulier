149304ecfed2d0a3b665157b32f5b496d74ba6f4
Mielesgames <emieltlam11@gmail.com>
2023-03-14 11:33:24 +0100

    I have no idea how to put the entered data back...

--- php-version/Validation_POST/main.php
+++ php-version/Validation_POST/main.php
@@ -11,21 +11,56 @@
     <?php
     if ($_POST != "" && $_POST != null){
         ?>
-        <h1>hi, this is an information page</h1>
-        <?php 
+        <?php
+        if ($_POST['fname'] == "" || $_POST['fname'] == null || $_POST['email'] == "" || $_POST['email'] == null){
+            ?>
+            <form method="post">
+            <label for="fname">First name:</label>
+            <?php
+                if ($_POST['fname'] == "" || $_POST['fname'] == null){
+                    print("*THIS FIELD IS REQUIRED");
+                }
+                ?><br>
+            <input type="text" id="fname" name="fname" value="" placeholder="John"><br>
+            <label for="mail">Maid:</label>
+            <?php
+                if ($_POST['email'] == "" || $_POST['email'] == null){
+                    print("*THIS FIELD IS REQUIRED");
+                }
+                else{
+                    ?>
+                    <script>
+                        document.getElementById("fname").value = FormData["fname"]
+                    </script>
+                    <?php
+                }
+                ?>
+                <br>
+            <input type="email" id="email" name="email" value="" placeholder="test@example.com"><br><br>
+            <input type="submit" value="Submit">
+            <?php
+        }
+        else{
 
-        echo htmlspecialchars("Name: " . $_POST['fname']);?><br><br>
-        <?php 
-        echo htmlspecialchars("Email: ". $_POST['email']);?>
+            ?>
+            <h1>hi, this is an information page</h1>
+            <?php 
+
+            echo htmlspecialchars("Name: " . $_POST['fname']);?><br><br>
+            <?php 
+            echo htmlspecialchars("Email: ". $_POST['email']);?>
+            <?php
+        }
+        ?>
         <?php
     }
     else{
         ?>
         <form method="post">
         <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="" placeholder="John" required><br>
+        <input type="text" id="fname" name="fname" value="" placeholder="John"><br>
         <label for="mail">Maid:</label><br>
-        <input type="text" id="email" name="email" value="" placeholder="test@example.com" required><br><br>
+        <input type="text" id="email" name="email" value="" placeholder="test@example.com"><br><br>
         <input type="submit" value="Submit">
     </form>
     <?php
