86d1829024b78d88175a6a0b599e57b33e062022
Mielesgames <emieltlam11@gmail.com>
2023-03-14 10:58:34 +0100

    security thingy

--- php-version/Validation_POST/main.php
+++ php-version/Validation_POST/main.php
@@ -9,23 +9,23 @@
 </head>
 <body>
     <?php
-    if ($_GET != "" && $_GET != null){
+    if ($_POST != "" && $_POST != null){
         ?>
         <h1>hi, this is an information page</h1>
         <?php 
 
-        echo "Name: " . $_GET['fname'];?><br><br>
+        echo htmlspecialchars("Name: " . $_POST['fname']);?><br><br>
         <?php 
-        echo "Email: ". $_GET['email'];?>
+        echo htmlspecialchars("Email: ". $_POST['email']);?>
         <?php
     }
     else{
         ?>
-        <form type="post">
+        <form method="post">
         <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="" placeholder="John"><br>
+        <input type="text" id="fname" name="fname" value="" placeholder="John" required><br>
         <label for="mail">Maid:</label><br>
-        <input type="text" id="email" name="email" value="" placeholder="test@example.com"><br><br>
+        <input type="text" id="email" name="email" value="" placeholder="test@example.com" required><br><br>
         <input type="submit" value="Submit">
     </form>
     <?php
